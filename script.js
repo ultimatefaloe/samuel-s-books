bea2745c8ced1039368b6c95f950084b87033e00
ultimatefaloe <ultimatefaloe@outlook.com>
2025-08-20 12:05:05 +0100

    pushing after fixing conflict

--- script.js
+++ script.js
@@ -1,24 +1,395 @@
-const editBtn = document.getElementById("edit_btn");
-const saveBtn = document.getElementById("save_btn");
-const form = document.getElementById('form')
-let btnChange = false;
+// const editBtn = document.getElementById("edit_btn");
+// const saveBtn = document.getElementById("save_btn");
+// const form = document.getElementById('form')
+// let btnChange = false;
 
-editBtn.addEventListener("click", (e) => {
-  editBtn.style.display = "none";
-  saveBtn.style.display = "block";
-  console.log("Button clicked");
-  console.log(btnChange);
-});
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// editBtn.addEventListener("click", (e) => {
+//   editBtn.style.display = "none";
+//   saveBtn.style.display = "block";
+//   console.log("Button clicked");
+//   console.log(btnChange);
+// });
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  editBtn.style.display = "block";
-  saveBtn.style.display = "none";
-  const fname = document.getElementById("firstName");
-  const lname = document.getElementById("lastName");
+//   editBtn.style.display = "block";
+//   saveBtn.style.display = "none";
+//   const fname = document.getElementById("firstName");
+//   const lname = document.getElementById("lastName");
 
-  console.log({
-    firstname: fname.value,
-    Lastname: lname.value,
-  });
-});
+//   console.log({
+//     firstname: fname.value,
+//     Lastname: lname.value,
+//   });
+// });
+
+const Person = {
+  lname: "ULTIMATE",
+  fname: "Faloe",
+  hobby: "Coding"
+}
+
+const DOM = document.getElementById('object')
+console.log(Person.hobby)
+
+Person.hobby = "Car Race"
+
+const Array = ["ULTIMATE", "FALOE", [ 12, 26, [ "Vibe"] ]]
+
+console.log(Array[2][2][0])
+
+const arrObj = [
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+  {
+    title: "Educational",
+    description: "This is meant for educational purpose",
+    price: 6500,
+    stock: 129,
+    images: ["image", "images1", "image2", "image3"]
+  },
+]
+
+console.log(arrObj[0].images[3])
+
+const html = 
+  arrObj.map(i => (
+    `
+      <div class="inspirational_section">
+                <div class="featured_grid">
+                    <div class="inspirational_header">
+                        <p>${i.title}</p>
+                    </div>
+                    <div class="inspirational_image">
+                        <img src="${i.images[0]}" alt="${i.images[0]}">
+                    </div>
+                </div>
+                <div class="inspirational_content">
+                    <div class="inspirational_title">
+                        <h2>${i.title}</h2>
+                    </div>
+                    <div class="inspirational_text">
+                        <p>
+                            One book can change your mindset.<br>One mindset can change your life.<br>Discover your next read now!     
+                        </p>
+                    </div>
+                </div>
+                <div class="inspirational_pricing_action">
+                    <div>
+                        <h3>₦35,000</h3>
+                    </div>
+                    <div>
+                        <button>Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+    `
+  ))
+
+DOM.innerHTML = html
